dabe60855086a20bafd4030be37a4f14628fea75
William Kennedy <alexkenned@gmail.com>
2020-02-27 19:42:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,9 +54,10 @@ end
 def reduce_to_all_true(source_array)
   i = 0 
   while i < source_array.length do 
-    return false if source_array[i] = false 
+    return true if source_array[i] = true 
     i+=1 
   end 
+  return false 
 end
 
 def reduce_to_any_true(source_array)
